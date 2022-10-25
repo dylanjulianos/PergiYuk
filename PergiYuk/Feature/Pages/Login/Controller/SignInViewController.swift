b883983fc4a7d1dc9a9e58f515c60bb96a819896
anastasiaagstn <agustine.anastasia@gmail.com>
2022-10-25 17:21:18 +0700

    Updated some stuff

--- PergiYuk/Feature/Pages/Login/Controller/SignInViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SignInViewController.swift
-//  PergiYuk
-//
-//  Created by Anastasia Agustine on 11/10/22.
-//
-
-import Foundation
--- PergiYuk/Feature/Pages/Sign Up/Controller/SignUpViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SignUpViewController.swift
-//  PergiYuk
-//
-//  Created by Anastasia Agustine on 11/10/22.
-//
-
-import Foundation
