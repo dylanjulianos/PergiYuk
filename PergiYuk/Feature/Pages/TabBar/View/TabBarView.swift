913c3d401e9997c0ebc7f7a496a441e4d843dbc4
dylanjulianos <103484220+dylanjulianos@users.noreply.github.com>
2022-11-01 12:44:00 +0700

    feat: Adding TabBar Folder

---
+++ PergiYuk/Feature/Pages/TabBar/View/TabBarView.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarView.swift
+//  PergiYuk
+//
+//  Created by Dylan Juliano Santoso on 01/11/22.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text("TabBar")
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
