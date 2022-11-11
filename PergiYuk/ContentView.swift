40f620cbbcfbc342ac1a1c745098d32492dd69b0
dylanjulianos <103484220+dylanjulianos@users.noreply.github.com>
2022-11-11 15:03:52 +0700

    feat: Fixing TabBarView

--- PergiYuk/ContentView.swift
+++ PergiYuk/ContentView.swift
@@ -6,48 +6,11 @@
 //
 
 import SwiftUI
-enum TabBarr {
-    case map
-    case paperplane
-    case person
-}
 
 struct ContentView: View {
-    
-    var selectedTab = TabBarr.person
 
     var body: some View {
-        
-//        VStack {
-//            switch selectedTab {
-//            case .map:
-//                UserAboutMeView()
-//
-//            case .paperplane:
-//                MyPartyView()
-//
-//            case .person:
-//                UserProfileView()
-//            }
-//            TabBarView()
-//        }
-        
-        VStack {
-            switch selectedTab{
-            case .map:
-                UserAboutMeView()
-                
-            case .person:
-                UserProfileView(currentProgress: 0)
-                
-            case .paperplane:
-                MyPartyView()
-            }
-            
-            TabBarView()
-        }
-        
-        
+      TabBarView()
     }
 }
 
