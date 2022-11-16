04dd034238b72129ae394be2f687cb357ad2f32a
dylanjulianos <103484220+dylanjulianos@users.noreply.github.com>
2022-11-16 14:26:49 +0700

    feat: Editing User Details View

--- PergiYuk/Feature/Pages/Profile/View/UserHistoryView.swift
+++ PergiYuk/Feature/Pages/Profile/View/UserHistoryView.swift
@@ -17,43 +17,47 @@ struct UserHistoryView: View {
     //MARK: - Lifecycle
     
     var body: some View {
-        
         GeometryReader { _ in
             VStack(alignment: .center) {
                 //Title for Feedback Rating
-                RoundedRectangle(cornerRadius: 20, style: .continuous)
-                    .foregroundColor(.white)
-                    .padding([.leading, .trailing])
-//                    .frame(width: 360, height: 125)
-                    .frame(width: width * 0.92, height: height * 0.15)
-                    .shadow(radius: 1)
-                    .overlay(
-                    Text("Feedback Rating")
-                        .font(.custom("Metropolis-SemiBold", size: 14))
-                        .offset(y: -28)
-                    )
-                Rectangle()
-                    .frame(width: 270, height: 1)
-                    .offset(y: -80)
-                    .foregroundColor(.gray)
+                VStack {
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .foregroundColor(.white)
+                        .padding([.leading, .trailing])
+                        .frame(width: width * 0.92, height: height * 0.15)
+                        .shadow(radius: 1)
+                        .overlay(
+                        Text("Feedback Rating")
+                            .font(.custom("Metropolis-SemiBold", size: 14))
+                            .offset(y: -width * 0.1)
+                            .background(
+                                Divider()
+                                    .frame(width: width * 0.7, height: 2)
+                                    .offset(y: -width * 0.06)
+                            )
+                        )
+                    .padding(.bottom)
+                }
                 
                 //Title for History
-                RoundedRectangle(cornerRadius: 20, style: .continuous)
-                    .foregroundColor(.white)
-                    .padding([.leading, .trailing])
-//                    .frame(width: 360, height: 125)
-                    .frame(width: width * 0.92, height: height * 0.15)
-                    .shadow(radius: 1)
-                    .overlay(
-                    Text("Trip History")
-                        .font(.custom("Metropolis-SemiBold", size: 14))
-                        .offset(y: -28)
-                    )
-                Rectangle()
-                    .frame(width: 270, height: 1)
-                    .offset(y: -80)
-                    .foregroundColor(.gray)
-                    
+                VStack {
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .foregroundColor(.white)
+                        .padding([.leading, .trailing])
+                        .frame(width: width * 0.92, height: height * 0.15)
+                        .shadow(radius: 1)
+                        .overlay(
+                        Text("History")
+                            .font(.custom("Metropolis-SemiBold", size: 14))
+                            .offset(y: -width * 0.1)
+                            .background(
+                                Divider()
+                                    .frame(width: width * 0.7, height: 2)
+                                    .offset(y: -width * 0.06)
+                            )
+                        )
+                    .padding(.bottom)
+                }
             }
             .padding()
         }
